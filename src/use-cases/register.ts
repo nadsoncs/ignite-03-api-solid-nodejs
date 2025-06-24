7f3a579d1133860cc22ad08a44187f4327f9c3e0
Nadson Cerqueira <nadsoncerqueira@gmail.com>
2025-06-24 18:25:54 -0300

    first commit

---
+++ src/use-cases/register.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/lib/prisma'
+import { PrismaUsersRepository } from '@/repositories/prisma.users.repository'
+import { hash } from 'bcryptjs'
+
+interface RegisterUseCaseParams {
+  name: string
+  email: string
+  password: string
+}
+export async function registerUseCase({
+  name,
+  email,
+  password,
+}: RegisterUseCaseParams) {
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('E-mail already exists')
+  }
+
+  const password_hash = await hash(password, 6)
+
+  const prismaUsersRepository = new PrismaUsersRepository()
+
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
+  })
+}
